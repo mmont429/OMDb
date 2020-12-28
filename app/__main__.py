c4b4e07b809067c68713de047b1c7a3c030dcbde
unknown <mmont429@gmail.com>
2020-12-27 21:17:07 -0500

    basicReact

--- app/__main__.py
+++
@@ -1,6 +0,0 @@
-from app import app, frontend
-
-FLASK_PORT = 5001
-
-if __name__ == "__main__":
-    app.run (debug=True, port=FLASK_PORT, host='127.0.0.1')
---
+++ app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React from 'react'
+import { SearchBar } from './components/search-bar/search-bar'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
